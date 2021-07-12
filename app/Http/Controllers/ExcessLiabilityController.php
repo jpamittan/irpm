bfff3f5d8cc553f3c4d8effe1021d7ae3bf79c9f
jayson-pamittan <jayson.pamittan@gmail.com>
2021-07-12 20:20:15 +0800

    Print debug in preprod temporarily.

--- app/Http/Controllers/ExcessLiabilityController.php
+++ app/Http/Controllers/ExcessLiabilityController.php
@@ -11,20 +11,21 @@ class ExcessLiabilityController extends Controller
 {
     public function index(Request $request)
     {
+        $response = $request->get('Response');
+        $response = json_decode($response, true);
+        $view = ($response['action'] == "Refer" || $response['action'] == "Quote") 
+            ? 'lob.el.success'
+            : 'lob.el.decline';
         if (isset($_GET['env'])) {
             if ($_GET['env'] == "preprod") {
                 config(['sqlsvr.connection' => 'sqlsrv_exl_pre']);
+                dd($response);
             } else if ($_GET['env'] == "production") {
                 config(['sqlsvr.connection' => 'sqlsrv_exl_prd']);
             }
         } else {
             config(['sqlsvr.connection' => 'sqlsrv_exl']);
         }
-        $response = $request->get('Response');
-        $response = json_decode($response, true);
-        $view = ($response['action'] == "Refer" || $response['action'] == "Quote") 
-            ? 'lob.el.success'
-            : 'lob.el.decline';
         $submission = Submission::where('submission_id', $response['submissionId'])
             ->first();
 
