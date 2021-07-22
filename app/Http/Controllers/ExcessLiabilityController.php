1d977fec0ad0e67edc5690da9a7b4952b0cad79f
jayson-pamittan <jayson.pamittan@gmail.com>
2021-07-22 08:50:14 +0800

    Add page if no submission if detected in th epost request.

--- app/Http/Controllers/ExcessLiabilityController.php
+++ app/Http/Controllers/ExcessLiabilityController.php
@@ -11,6 +11,7 @@ class ExcessLiabilityController extends Controller
 {
     public function index(Request $request)
     {
+        $view = 'lob.el.success';
         $response = $request->get('Response');
         $response = json_decode($response, true);
         if (isset($_GET['env'])) {
@@ -22,18 +23,21 @@ class ExcessLiabilityController extends Controller
         } else {
             config(['sqlsvr.connection' => 'sqlsrv_exl']);
         }
-        $submission = Submission::where('submission_id', $response['submissionId'])
-            ->with('latestSubmissionMods')
-            ->first();
-        $view = 'lob.el.success';
-        if ($submission->latestSubmissionMods) {
-            if ($submission->latestSubmissionMods->outcome_type_id == 2) {
-                // Refer
-                $view = 'lob.el.success';
-            } else if ($submission->latestSubmissionMods->outcome_type_id == 3) {
-                // Decline
-                $view = 'lob.el.decline';
+        if (! empty($response['submissionId'])) {
+            $submission = Submission::where('submission_id', $response['submissionId'])
+                ->with('latestSubmissionMods')
+                ->first();
+            if ($submission->latestSubmissionMods) {
+                if ($submission->latestSubmissionMods->outcome_type_id == 2) {
+                    // Refer
+                    $view = 'lob.el.success';
+                } else if ($submission->latestSubmissionMods->outcome_type_id == 3) {
+                    // Decline
+                    $view = 'lob.el.decline';
+                }
             }
+        } else {
+            $view = 'lob.el.error';
         }
 
         return view($view, [
---
+++ resources/views/lob/el/error.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.auth')
+
+@section('content')
+    <div class="container" id="login-form">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <a href="#" class="login-logo"><img src="{{ asset('img/synchronosure_blue_trans_logo.png') }}"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="alert alert-dismissable alert-primary">
+					<h3><i class="fas fa-exclamation-triangle"></i> Ooops</h3> 
+					<p>
+                        No submission ID. If you cancelled the submission please try again.
+                    </p>
+				</div>
+            </div>
+        </div>
+    </div>
+@endsection
